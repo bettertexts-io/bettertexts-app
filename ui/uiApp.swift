956dd45a5c85795074f8f5e644564350150488c7
leolurch <40560457+leolurch@users.noreply.github.com>
2023-01-25 09:10:31 +0000

    Initial Commit

---
+++ ui/uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  uiApp.swift
+//  ui
+//
+//  Created by Leo Grützner on 25.01.23.
+//
+
+import SwiftUI
+
+@main
+struct uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
